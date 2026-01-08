7adc8e70834dae258635677a8767aff760dd7a2c
pedro <pedromunhozrosin@gmail.com>
2026-01-08 09:25:39 -0300

    global instance axios

---
+++ axios/js/global.js
@@ -0,0 +1,3 @@
+//configura valores padrões do axios para todas as requisicoes:
+axios.defaults.headers.common["Accept"] = "application/json";
+axios.defaults.headers.common["Authorization"] = "MEUTOKENDOAPP";
