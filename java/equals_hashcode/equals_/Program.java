77e25ec6493d4ec353d58cd27929b6cdc186a423
pedro <pedromunhozrosin@gmail.com>
2026-02-26 15:43:24 -0300

    iniciando estudos de equals e hashcode

---
+++ java/equals_hashcode/equals_/Program.java
@@ -0,0 +1,12 @@
+package equals_hashcode.equals_;
+
+public class Program {
+    public static void main(String[] args) {
+        String a = "Maria";
+        String b = "Alex";
+
+        System.out.println(a.equals(b));
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+    }
+}
