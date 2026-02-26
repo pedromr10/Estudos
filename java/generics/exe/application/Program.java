515bca3892dcc29e947b89ebfe570315a251ae03
pedro <pedromunhozrosin@gmail.com>
2026-02-26 10:36:46 -0300

    adicionando estudo generic delimitado

---
+++ java/generics/exe/application/Program.java
@@ -0,0 +1,33 @@
+package generics.exe.application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import generics.exe.model.entities.Product;
+import generics.exe.model.services.CalculationService;
+
+public class Program {
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+
+        String path = "generics\\exe\\in.txt";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+            String line = br.readLine();
+            while(line != null){
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
+
+            Product x = CalculationService.max(list);
+            System.out.println("Max: " + x);
+        }
+        catch(IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
---
+++ java/generics/exe/model/services/CalculationService.java
@@ -0,0 +1,19 @@
+package generics.exe.model.services;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CalculationService {
+    public static <T extends Comparable<T>> T max(List<T> list){ //metodo trabalha com tipo T desde que seja qualquer subtipo de comparable T
+        if(list.isEmpty()){
+            throw new IllegalStateException("List can't be emoty");
+        }
+        T max = list.get(0);
+        for(T item : list){
+            if(item.compareTo(max) > 0){ //se for true, retorna numero maior que zero, pelo que me lembro
+                max = item;
+            }
+        }
+        return max;
+    }
+}
