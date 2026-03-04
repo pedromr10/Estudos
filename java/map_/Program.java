d63017948c831c0f907cbc8e0608fff57cb6865c
pedro <pedromunhozrosin@gmail.com>
2026-03-04 10:43:17 -0300

    inciando estudo de map

---
+++ java/map_/Program.java
@@ -0,0 +1,23 @@
+package map_;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program {
+    public static void main(String[] args) {
+        Map<String, String> cookies = new TreeMap<>();
+
+        //inserir usando put:
+        cookies.put("username", "Pedro"); //chave, valor
+        cookies.put("email", "pedro@gmail.com"); //chave, valor
+        cookies.put("phone", "119999999"); //chave, valor
+
+        cookies.remove("email");
+        cookies.put("phone", "118888888");
+
+        for(String key : cookies.keySet()){ //keyset pega as chaves do map
+            System.out.println(key + " : " + cookies.get(key)); //treemap ordenou por string (alfabetica)
+        }
+        
+    }
+}
