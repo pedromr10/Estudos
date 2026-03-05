ccd4178bc8d0c4e819f31aaf362a40cebdc13fd4
pedro <pedromunhozrosin@gmail.com>
2026-03-05 10:43:56 -0300

    adicionandoe estudo predicate - exercicio ainda sem predicate

---
+++ java/programFuncional_lambda/Predicate_/application/Program.java
@@ -0,0 +1,24 @@
+package programFuncional_lambda.Predicate_.application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import programFuncional_lambda.Predicate_.model.entities.Product;
+
+public class Program {
+    public static void main(String[] args) {
+        List <Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.00));
+
+        //tirando os produtos que custam mais de 100 reais:
+        list.removeIf(p -> p.getPrice() >= 100);
+
+        for(Product p : list){
+            System.out.println(p);
+        }
+    }
+}
