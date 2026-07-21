d6570fa87191dff2b920dbdae2e4a6fb4255021d
pedro <pedromunhozrosin@gmail.com>
2026-07-21 13:24:14 -0300

    added hello world

--- springBoot/2-primeiros_passos/produtosapi/src/main/java/com/pedro/produtosapi/ProdutosapiApplication.java
+++ springBoot/2-primeiros_passos/produtosapi/src/main/java/com/pedro/produtosapi/ProdutosapiApplication.java
@@ -2,9 +2,17 @@ package com.pedro.produtosapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ProdutosapiApplication {
+	
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutosapiApplication.class, args);
