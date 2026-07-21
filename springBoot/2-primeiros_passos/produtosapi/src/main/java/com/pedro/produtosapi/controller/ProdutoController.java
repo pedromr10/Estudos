e7fb29414720e52bb73748a27cfeed41054722b6
pedro <pedromunhozrosin@gmail.com>
2026-07-21 13:51:25 -0300

    added produto controller

--- springBoot/2-primeiros_passos/produtosapi/src/main/java/com/pedro/produtosapi/controller/ProdutoController.java
+++ springBoot/2-primeiros_passos/produtosapi/src/main/java/com/pedro/produtosapi/controller/ProdutoController.java
@@ -1,5 +1,20 @@
 package com.pedro.produtosapi.controller;
 
-public class ProdutoController {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.pedro.produtosapi.model.Produto;
+
+@RestController //recebe requisicoes web
+@RequestMapping("/produtos")
+public class ProdutoController {
+	
+	//salvar produto
+	@PostMapping
+	public Produto salvar(@RequestBody Produto produto) {
+		System.out.println("Produto recebido: " + produto);
+		return produto;
+	}
 }
--- springBoot/2-primeiros_passos/produtosapi/src/main/java/com/pedro/produtosapi/model/Produto.java
+++ springBoot/2-primeiros_passos/produtosapi/src/main/java/com/pedro/produtosapi/model/Produto.java
@@ -2,9 +2,11 @@ package com.pedro.produtosapi.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 
 public class Produto {
 	private String id;
