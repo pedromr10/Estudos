4ec085d7b176f10ccef141175cf20d846c69365e
pedro <pedromunhozrosin@gmail.com>
2026-07-27 14:53:51 -0300

    fix: add jpa annotations

--- springBoot/2-primeiros_passos/produtosapi/src/main/java/com/pedro/produtosapi/model/Produto.java
+++ springBoot/2-primeiros_passos/produtosapi/src/main/java/com/pedro/produtosapi/model/Produto.java
@@ -1,5 +1,9 @@
 package com.pedro.produtosapi.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,9 +12,18 @@ import lombok.ToString;
 @Setter
 @ToString
 
+@Entity
+@Table(name = "produto")
 public class Produto {
+	@Id
+	@Column
 	private String id;
+	@Column
 	private String nome;
+	@Column
 	private String descricao;
+	@Column
 	private Double preco;
+	//obs: caso no banco de dados o nome da propriedade esteja diferente, pode 
+	// referencia-la assim: @Column(name = "nomeNoBanco"), o mesmo para a anotation @Table
 }
