43cca1988c30c217a0d86c6136623ab3dcc91b3b
pedro <pedromunhozrosin@gmail.com>
2026-07-30 11:36:56 -0300

    added project

---
+++ springBoot/3-arquitetura_e_estrutura/arquiteturaspring/src/main/java/com/pedro/arquiteturaspring/ArquiteturaspringApplication.java
@@ -0,0 +1,18 @@
+package com.pedro.arquiteturaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class ArquiteturaspringApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(ArquiteturaspringApplication.class, args);
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(ArquiteturaspringApplication.class);
+		
+		//starta o projeto:
+		builder.run(args);
+	}
+
+}
