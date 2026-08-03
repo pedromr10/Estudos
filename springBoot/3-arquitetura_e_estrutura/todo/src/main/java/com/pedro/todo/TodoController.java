b312594e61f8da72d0fe382ca0ad68b021505c82
pedro <pedromunhozrosin@gmail.com>
2026-08-03 14:42:06 -0300

    aula: demonstrando a arquitetura em funcionamento

--- springBoot/3-arquitetura_e_estrutura/todo/src/main/java/com/pedro/todo/TodoController.java
+++ springBoot/3-arquitetura_e_estrutura/todo/src/main/java/com/pedro/todo/TodoController.java
@@ -1,6 +1,9 @@
 package com.pedro.todo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +20,16 @@ public class TodoController {
 	@PostMapping
 	public void salvar(@RequestBody TodoEntity todo) {
 		this.service.salvar(todo);
+	}
+	
+	@PutMapping("{id}")
+	public void atualizarStatus(@PathVariable("id") Integer id, @RequestBody TodoEntity todo) {
+		todo.setId(id);
+		service.atualizarStatus(todo);
+	}
+	
+	@GetMapping("{id")
+	public TodoEntity buscarPorId(@PathVariable("id") Integer id) {
+		return service.buscarPorId(id);
 	}
 }
--- springBoot/3-arquitetura_e_estrutura/todo/src/main/java/com/pedro/todo/TodoService.java
+++ springBoot/3-arquitetura_e_estrutura/todo/src/main/java/com/pedro/todo/TodoService.java
@@ -15,4 +15,12 @@ public class TodoService {
 	public TodoEntity salvar(TodoEntity novoTodo) {
 		return repository.save(novoTodo);
 	}
+	
+	public void atualizarStatus(TodoEntity todo) {
+		repository.save(todo);
+	}
+	
+	public TodoEntity buscarPorId(Integer id) {
+		return repository.findById(id).orElse(null);
+	}
 }
