0fa56fed092c775487ec721ec618bc00497156a4
pedro <pedromunhozrosin@gmail.com>
2026-08-04 14:14:54 -0300

    aula: inicializacao lazy dos beans

--- springBoot/3-arquitetura_e_estrutura/todo/src/main/java/com/pedro/todo/TodoApplication.java
+++ springBoot/3-arquitetura_e_estrutura/todo/src/main/java/com/pedro/todo/TodoApplication.java
@@ -2,12 +2,16 @@ package com.pedro.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TodoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
+		//SpringApplication.run(TodoApplication.class, args);
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(TodoApplication.class);
+		
+		builder.lazyInitialization(true); //quando colocado como true, todos os beans terao a annotation @Lazy por padrao. tomar cuidado com essa funcionalidade, pois pode omitir erros que serao acionados em runtime
 	}
 
 }
--- springBoot/3-arquitetura_e_estrutura/todo/src/main/java/com/pedro/todo/TodoValidator.java
+++ springBoot/3-arquitetura_e_estrutura/todo/src/main/java/com/pedro/todo/TodoValidator.java
@@ -1,7 +1,9 @@
 package com.pedro.todo;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+@Lazy(true) //quando true, bean so sera instanciado quando for utiliza-lo
 @Component //component "eh a annotation pai de todos", de service, controller..
 //obs: o component tem como escopo singleton, ou seja, por padrao, quando o Spring cria um bean, ele cria apenas uma instancia e reutiliza essa mesma instancia sempre que alguem precisar dela. para gerenciar o escopo, tem a annotation @Scope("tipoEscopo")
 public class TodoValidator {
