95256a4f8e3401b8a6fbc3fad6a378096dbd7dcb
pedro <pedromunhozrosin@gmail.com>
2026-08-04 14:00:45 -0300

    aula: escopo dos beans com @scope

--- springBoot/3-arquitetura_e_estrutura/todo/src/main/java/com/pedro/todo/TodoValidator.java
+++ springBoot/3-arquitetura_e_estrutura/todo/src/main/java/com/pedro/todo/TodoValidator.java
@@ -3,6 +3,7 @@ package com.pedro.todo;
 import org.springframework.stereotype.Component;
 
 @Component //component "eh a annotation pai de todos", de service, controller..
+//obs: o component tem como escopo singleton, ou seja, por padrao, quando o Spring cria um bean, ele cria apenas uma instancia e reutiliza essa mesma instancia sempre que alguem precisar dela. para gerenciar o escopo, tem a annotation @Scope("tipoEscopo")
 public class TodoValidator {
 	
 	private TodoRepository repository;
